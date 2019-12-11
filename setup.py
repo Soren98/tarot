3d02d162b822d46f096cebdcfb5430d69f0fc940
Søren <thesecondsoren@gmail.com>
2019-12-11 16:52:03 -0500

    stable (v1.0). manages a tarot card deck with functions draw, reset, and check number of cards. still lacks save and load function for keeping state when the bot is turned off.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='tarot',
+    version='1.0',
+    packages=['tarot'],
+    url='https://github.com/Soren98/dip',
+    license='Unlicense',
+    author='Søren',
+    author_email='sorenthedesolate@gmail.com',
+    description='a discord bot that manages a virtual tarot card deck.',
+    install_requires=['discord.py'],
+    requires_python='>=3.6.0'
+)
---
+++ tarobot.py
@@ -0,0 +1,61 @@
+import sys
+
+from discord import Client, File, DMChannel
+
+from tarot_deck import TarotDeck
+
+client = Client()
+tarot_deck = TarotDeck()
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user or isinstance(message.channel, DMChannel):
+        return
+    auth = str(message.author)
+    content = str(message.content)
+    channel = message.channel
+    # hardcoded so only i can kill the bot
+    if content == '!tarot kill' and message.author.id == 480300800518782996:
+        await client.logout()
+    elif content == '!tarot help':
+        await channel.send(
+            "commands are: **!tarot draw**, **!tarot reset**, **!tarot cards left**, **!hello**\n"
+            "add a number to the draw command to draw multiple cards. ex: **!tarot draw 3**")
+    elif content == '!hello':
+        print('{} greet'.format(auth))
+        await channel.send('Fuck off {}, ya cunt!\n'.format(str(auth)))
+    elif content.startswith('!tarot draw'):
+        num_cards = 1
+        try:
+            if len(content) > 12:
+                num_cards = int(content[12:])
+        except ValueError:
+            await channel.send("{} ain't a number, idiot".format(content[12:]))
+            return
+        print('{} draw {}'.format(auth, num_cards))
+        for _ in range(num_cards):
+            reply, attachment = tarot_deck.draw()
+            if attachment:
+                await channel.send(reply, file=File(attachment))
+            else:
+                await channel.send(reply)
+                break
+    elif content == '!tarot reset':
+        print('{} reset'.format(auth))
+        await channel.send(tarot_deck.reset())
+    elif content == '!tarot cards left':
+        print('{} checked how many cards left'.format(auth))
+        await channel.send('there are {} cards left in the tarot deck'.format(len(tarot_deck.deck)))
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+assert len(sys.argv) == 2, "must provide only one input, the bot's token"
+TOKEN = sys.argv[1]
+client.run(TOKEN)
